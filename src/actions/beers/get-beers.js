98869dacd7d7fc553fe53fa862eb9066e154cfe2
maressajoseph <maressa.joseph@gmail.com>
2017-07-23 22:44:26 +0200

    Last modifications on styling + moved the search bar query from api request to just showing/hiding items on frontend

--- src/actions/beers/get-beers.js
+++ src/actions/beers/get-beers.js
@@ -11,49 +11,25 @@ export default ({ query } = {}) => {
   return (dispatch) => {
     dispatch({ type: APP_LOADING })
 
-      if (query) {
-        fetch(`http://api.brewerydb.com/v2/beers?styleId=3&name=${query}&order=ibu&hasLabels=Y&key=e3710d383a093f086152b9af977123ce`, {
-          method: 'GET'
-        }).then(response => response.json())
-        .then((json) => {
-          dispatch({ type: APP_DONE_LOADING })
-          dispatch({ type: LOAD_SUCCESS })
+      fetch(`http://api.brewerydb.com/v2/beers?styleId=2&withBreweries=Y&order=ibu&hasLabels=Y&key=e3710d383a093f086152b9af977123ce`, {
+        method: 'GET'
+      }).then(response => response.json())
+      .then((json) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
 
-          dispatch({
-            type: FETCHED_BEERS,
-            payload: json.data
-          })
+        dispatch({
+          type: FETCHED_BEERS,
+          payload: json.data
         })
-        .catch((error) => {
-          dispatch({ type: APP_DONE_LOADING })
-          dispatch({
-            type: LOAD_ERROR,
-            payload: error.message
-          })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
         })
-      } else {
-        fetch(`http://api.brewerydb.com/v2/beers?styleId=3&order=ibu&hasLabels=Y&key=e3710d383a093f086152b9af977123ce`, {
-          method: 'GET'
-        }).then(response => response.json())
-        .then((json) => {
-          dispatch({ type: APP_DONE_LOADING })
-          dispatch({ type: LOAD_SUCCESS })
-
-          dispatch({
-            type: FETCHED_BEERS,
-            payload: json.data
-          })
-        })
-        .catch((error) => {
-          dispatch({ type: APP_DONE_LOADING })
-          dispatch({
-            type: LOAD_ERROR,
-            payload: error.message
-          })
-        })
-      }
-
-
-  }
+      })
+    }
 
 }
--- src/components/BeerContainer.js
+++ src/components/BeerContainer.js
@@ -3,28 +3,50 @@ import { connect } from 'react-redux'
 import './BeerContainer.css'
 
 export class BeerContainer extends PureComponent {
+
   render() {
     const { beers } = this.props
-    return (
-      <div className="allbeers">
-        {beers.map((b, i) => {
-          return (
-            <div key={i} className="onebeer">
-              <img src={b.labels.medium} alt="beer" className="beerimage"/>
-              <h3>{b.name.toUpperCase()}</h3>
-              <div className="line"></div>
-              <p>{b.abv}%</p>
-              <div id="popup">{b.description}</div>
-            </div>
-          )
+    const { query } = this.props.searchQuery
 
-        })}
-      </div>
-    )
+    if (query) {
+      const beerswithquery = beers.filter((beer) => beer.name.toLowerCase().includes(query) )
+  
+      return (
+        <div className="allbeers">
+          {beerswithquery.map((b, i) => {
+            return (
+              <div key={i} className="onebeer">
+                <img src={b.labels.medium} alt="beer" className="beerimage"/>
+                <h3 className="beername">{b.name.toUpperCase()}</h3>
+                <div className="line"></div>
+                <p>{b.abv}%</p>
+                <div id="popup">{b.description}</div>
+              </div>
+            )
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div className="allbeers">
+          {beers.map((b, i) => {
+            return (
+              <div key={i} className="onebeer">
+                <img src={b.labels.medium} alt="beer" className="beerimage"/>
+                <h3>{b.name.toUpperCase()}</h3>
+                <div className="line"></div>
+                <p>{b.abv}%</p>
+                <div id="popup">{b.description}</div>
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
   }
 }
 
 
-const mapStateToProps = ({ beers }) => ({ beers })
+const mapStateToProps = ({ beers, searchQuery }) => ({ beers, searchQuery })
 
 export default connect(mapStateToProps)(BeerContainer)
